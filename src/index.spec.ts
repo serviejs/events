3325211e6dde4522d3dab9932c9527bc1035041f
Blake Embrey <hello@blakeembrey.com>
2021-04-08 18:56:08 -0700

    Remove old tinyevents name

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,122 +1,120 @@
 import { Emitter, once } from "./index";
 
-describe("tiny events", () => {
-  interface Events {
-    test: [boolean];
-    other: [string];
-  }
+interface Events {
+  test: [boolean];
+  other: [string];
+}
 
-  describe("events", () => {
-    it("should emit events", () => {
-      const events = new Emitter<Events>();
-      const spy = jest.fn();
+describe("events", () => {
+  it("should emit events", () => {
+    const events = new Emitter<Events>();
+    const spy = jest.fn();
 
-      const off = events.on("test", spy);
-      events.emit("test", true);
+    const off = events.on("test", spy);
+    events.emit("test", true);
 
-      off();
-      events.emit("test", false);
+    off();
+    events.emit("test", false);
 
-      expect(spy).toHaveBeenCalledTimes(1);
-      expect(spy).toHaveBeenLastCalledWith(true);
-    });
-
-    it("should remove function only once", () => {
-      const events = new Emitter<Events>();
-      const spy = jest.fn();
-
-      const off1 = events.on("test", spy);
-      const off2 = events.on("test", spy);
-      events.emit("test", true);
-      expect(spy).toHaveBeenCalledTimes(2);
-
-      off1();
-      off1();
-
-      events.emit("test", true);
-      expect(spy).toHaveBeenCalledTimes(3);
-
-      off2();
-
-      events.emit("test", false);
-      expect(spy).toHaveBeenCalledTimes(3);
-    });
-
-    it("should emit `each` event", () => {
-      const events = new Emitter<Events>();
-      const spy = jest.fn();
-
-      events.emit("test", false);
-      expect(spy).not.toHaveBeenCalled();
-
-      const off = events.each(spy);
-      events.emit("test", true);
-      expect(spy).toHaveBeenCalledTimes(1);
-
-      off();
-      events.emit("test", false);
-      expect(spy).toHaveBeenCalledTimes(1);
-    });
-
-    it("should forward `each` events easily", () => {
-      const events1 = new Emitter<Events>();
-      const events2 = new Emitter<Pick<Events, "test">>();
-      const spy = jest.fn();
-
-      events2.each(({ type, args }) => {
-        events1.emit(type, ...args);
-      });
-
-      events1.on("test", spy);
-      events2.emit("test", true);
-
-      expect(spy).toHaveBeenLastCalledWith(true);
-    });
-
-    it("should filter `each` events easily", () => {
-      const events1 = new Emitter<Events>();
-      const events2 = new Emitter<Pick<Events, "test">>();
-      const spy = jest.fn();
-
-      events1.each((e) => {
-        if (e.type === "test") {
-          events2.emit(e.type, ...e.args);
-        }
-      });
-
-      events2.on("test", spy);
-      events1.emit("test", true);
-      events1.emit("other", "");
-
-      expect(spy).toHaveBeenLastCalledWith(true);
-    });
+    expect(spy).toHaveBeenCalledTimes(1);
+    expect(spy).toHaveBeenLastCalledWith(true);
   });
 
-  describe("once", () => {
-    it("should support a once listener", () => {
-      const events = new Emitter<Events>();
-      const spy = jest.fn();
+  it("should remove function only once", () => {
+    const events = new Emitter<Events>();
+    const spy = jest.fn();
 
-      once(events, "test", spy);
+    const off1 = events.on("test", spy);
+    const off2 = events.on("test", spy);
+    events.emit("test", true);
+    expect(spy).toHaveBeenCalledTimes(2);
 
-      events.emit("test", true);
-      events.emit("test", false);
+    off1();
+    off1();
 
-      expect(spy).toHaveBeenCalledTimes(1);
+    events.emit("test", true);
+    expect(spy).toHaveBeenCalledTimes(3);
+
+    off2();
+
+    events.emit("test", false);
+    expect(spy).toHaveBeenCalledTimes(3);
+  });
+
+  it("should emit `each` event", () => {
+    const events = new Emitter<Events>();
+    const spy = jest.fn();
+
+    events.emit("test", false);
+    expect(spy).not.toHaveBeenCalled();
+
+    const off = events.each(spy);
+    events.emit("test", true);
+    expect(spy).toHaveBeenCalledTimes(1);
+
+    off();
+    events.emit("test", false);
+    expect(spy).toHaveBeenCalledTimes(1);
+  });
+
+  it("should forward `each` events easily", () => {
+    const events1 = new Emitter<Events>();
+    const events2 = new Emitter<Pick<Events, "test">>();
+    const spy = jest.fn();
+
+    events2.each(({ type, args }) => {
+      events1.emit(type, ...args);
     });
 
-    it("should not break emitter by removing element within listener", () => {
-      const events = new Emitter<Events>();
-      const onSpy = jest.fn();
-      const onceSpy = jest.fn();
+    events1.on("test", spy);
+    events2.emit("test", true);
 
-      once(events, "test", onceSpy);
-      events.on("test", onSpy);
+    expect(spy).toHaveBeenLastCalledWith(true);
+  });
 
-      events.emit("test", true);
+  it("should filter `each` events easily", () => {
+    const events1 = new Emitter<Events>();
+    const events2 = new Emitter<Pick<Events, "test">>();
+    const spy = jest.fn();
 
-      expect(onSpy).toHaveBeenCalledTimes(1);
-      expect(onceSpy).toHaveBeenCalledTimes(1);
+    events1.each((e) => {
+      if (e.type === "test") {
+        events2.emit(e.type, ...e.args);
+      }
     });
+
+    events2.on("test", spy);
+    events1.emit("test", true);
+    events1.emit("other", "");
+
+    expect(spy).toHaveBeenLastCalledWith(true);
+  });
+});
+
+describe("once", () => {
+  it("should support a once listener", () => {
+    const events = new Emitter<Events>();
+    const spy = jest.fn();
+
+    once(events, "test", spy);
+
+    events.emit("test", true);
+    events.emit("test", false);
+
+    expect(spy).toHaveBeenCalledTimes(1);
+  });
+
+  it("should not break emitter by removing element within listener", () => {
+    const events = new Emitter<Events>();
+    const onSpy = jest.fn();
+    const onceSpy = jest.fn();
+
+    once(events, "test", onceSpy);
+    events.on("test", onSpy);
+
+    events.emit("test", true);
+
+    expect(onSpy).toHaveBeenCalledTimes(1);
+    expect(onceSpy).toHaveBeenCalledTimes(1);
   });
 });
